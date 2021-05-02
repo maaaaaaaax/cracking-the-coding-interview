2ddd80a95830caeffe64aaa990b6e3bb4ff5b536
Max Wiederholt <max.wiederholt@gmail.com>
2021-05-02 10:09:28 -0700

    Solved 1.7 Rotate Matrix

--- 1-7-rotate-matrix.py
+++ 1-7-rotate-matrix.py
@@ -28,7 +28,16 @@ odd_matrix = [
 # Start with a brute force, inefficient solution
 def rotate_matrix(matrix):
     # Create a new matrix so we don't overwrite original data
-    new_matrix = matrix[:]
+    length = len(matrix)
+    new_matrix = []
+    for row in range(0, length):
+        row_data = []
+        for column in range(0, length):
+            row_data.append(0)
+        new_matrix.append(row_data)
+    # print(length)
+    # print(new_matrix)
+    # return
     # Set a variable to track the length of the current matrix, removing one layer of the onion each iteration
     i = len(matrix)
     while i > 1:
@@ -41,26 +50,35 @@ def rotate_matrix(matrix):
             print("First row: ")
             for k in range(0, i - 1):
                 print(matrix[j][k+j])
+                new_matrix[j][k+j+1] = matrix[j][k+j]
             # Print last column
             print("Last column: ")
             for k in range(0, i - 1):
-                print(matrix[j + k][i - 1 + j]) 
+                print(matrix[j + k][i - 1 + j])
+                new_matrix[j + k + 1][i - 1 + j] = matrix[j + k][i - 1 + j] 
             # Print last row, reversed
             print("Last row, reversed: ")
             for k in reversed(range(1, i)):
                 print(matrix[i - 1 + j][k+j])
+                new_matrix[i - 1 + j][k+j - 1] = matrix[i - 1 + j][k+j]
             # Print first column, reversed
             print("First column, reversed: ")
             for k in reversed(range(1, i)):
-                print(matrix[k + j][0 + j])         
-            j += 1
+                print(matrix[k + j][0 + j])
+                new_matrix[k + j - 1][0 + j] = matrix[k + j][0 + j]
             if i % 2 == 0:
                 i = i / 2
             elif math.floor((i / 2)) == 1:
                 i = 1
             else:
                 i = int(math.floor((i / 2)) + 1)
-
+            if i == 1 and length % 2 == 1:
+                print('j is ' + str(j))
+                print('i is ' + str(i))
+                center = int(math.floor((length / 2)))
+                new_matrix[center][center] = matrix[center][center]
+            j += 1
+    return new_matrix
 # Output:
 # 4
 # 0
@@ -70,4 +88,4 @@ def rotate_matrix(matrix):
 # 5
 # 6
 
-rotate_matrix(odd_matrix)
+print(rotate_matrix(even_matrix))
