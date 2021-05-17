2c4ace588ab7f456db864db9893f93ca9643eb1a
Max Wiederholt <max.wiederholt@gmail.com>
2021-05-17 10:32:21 -0700

    Solved 1.5 One Away with JS

--- 1-5-one-away.js
+++ 1-5-one-away.js
@@ -11,8 +11,14 @@ function is_one_away(string_1, string_2)
     {
         return false; 
     }
-
-    return true;
+    if (is_one_replacement_away(string_1, string_2) || is_one_insert_away(string_1, string_2) || is_one_removal_away(string_1, string_2))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 function is_one_replacement_away(string_1, string_2)
@@ -39,5 +45,90 @@ function is_one_replacement_away(string_1, string_2)
     return true;
 }
 
+function is_one_insert_away(string_1, string_2)
+{
+    if (string_1.length != (string_2.length - 1))
+    {
+        return false;
+    }
+    let cursor_1 = 0;
+    let cursor_2 = 0;
+    let incremented = false;
+    while (cursor_1 < string_1.length)
+    {
+        if (string_1[cursor_1] != string_2[cursor_2])
+        {
+            if (incremented)
+            {
+                return false;
+            }
+            else
+            {
+                incremented = true;
+                cursor_2++;
+            }
+        }
+        else
+        {
+            cursor_1++;
+            cursor_2++;
+        }
+    }
+    return true;    
+}
+
+function is_one_removal_away(string_1, string_2)
+{
+    if (string_1.length != (string_2.length + 1))
+    {
+        return false;
+    }
+    let cursor_1 = 0;
+    let cursor_2 = 0;
+    let incremented = false;
+    while (cursor_1 < string_1.length)
+    {
+        if (string_1[cursor_1] != string_2[cursor_2])
+        {
+            if (incremented)
+            {
+                return false;
+            }
+            else
+            {
+                incremented = true;
+                cursor_1++;
+            }
+        }
+        else
+        {
+            cursor_1++;
+            cursor_2++;
+        }
+    }
+    return true;    
+}
+
 console.log(is_one_replacement_away("sell", "hell"));  // true
 console.log(is_one_replacement_away("hell", "hello"));  // false
+
+console.log(" ");
+
+console.log(is_one_insert_away("owl", "howl"));  // true
+console.log(is_one_insert_away("hell", "hello"));  // true
+console.log(is_one_insert_away("hello", "hello"));  // false
+console.log(is_one_insert_away("maxw", "max"));  // false
+
+console.log(" ");
+
+console.log(is_one_removal_away("howl", "owl"));  // true
+console.log(is_one_removal_away("hello", "hell"));  // true
+console.log(is_one_removal_away("hello", "hello"));  // false
+console.log(is_one_removal_away("max", "maxw"));  // false
+
+console.log(" ");
+
+console.log(is_one_away("sell", "hell"));  // true
+console.log(is_one_away("owl", "howl"));  // true
+console.log(is_one_away("hello", "hell"));  // true
+console.log(is_one_away("hello", "world"));  // false
